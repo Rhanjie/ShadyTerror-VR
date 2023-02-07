3d948d6ff9f7db4f2d0c6d4aa039ab4119b06391
RhAnjiE <irhanjie@gmail.com>
2023-02-07 04:28:58 +0100

    Add random ambient sounds for audio manager and refactor class

--- Assets/Scripts/Characters/AudioManager.cs
+++ Assets/Scripts/Characters/AudioManager.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace Characters
@@ -15,28 +17,77 @@ namespace Characters
     [RequireComponent(typeof(AudioSource))]
     public class AudioManager : MonoBehaviour
     {
-        [SerializeField] 
-        private List<ClipInfo> sounds;
-        private AudioSource _audioSource;
+        [FormerlySerializedAs("sounds")]
+        [SerializeField] private List<ClipInfo> callableSounds;
+        [SerializeField] private List<AudioClip> ambientSounds;
         
+        [SerializeField] [Range(0, 100)] private float minPauseTime = 2f;
+        [SerializeField] [Range(0, 100)] private float maxPauseTime = 4f;
+        
+        private AudioSource _audioSource;
+
+        private void OnValidate()
+        {
+            if (minPauseTime > maxPauseTime)
+                minPauseTime = maxPauseTime - 0.1f;
+        }
+
         private void Start()
         {
             _audioSource = GetComponent<AudioSource>();
+
+            if (ambientSounds.Count > 0)
+            {
+                StartCoroutine(UpdateAmbient());
+            }
+        }
+        
+        private IEnumerator UpdateAmbient()
+        {
+            while (true)
+            {
+                var randomOffset = Random.Range(minPauseTime, maxPauseTime);
+                
+                yield return new WaitWhile(() => _audioSource.isPlaying);
+                yield return new WaitForSeconds(randomOffset);
+                
+                var randomIndex = Random.Range(0, ambientSounds.Count);
+                PlayAmbientSound(randomIndex);
+            }
+
+            yield return null;
+        }
+        
+        private void PlayAmbientSound(int index)
+        {
+            if (index < 0 || index >= ambientSounds.Count)
+                throw new IndexOutOfRangeException("Wrong index for ambientSound!");
+            
+            var audioClip = ambientSounds[index];
+            PlaySoundInternal(audioClip, 1f, 1f, false);
         }
 
         public void PlaySound(string clipName)
         {
             PlaySoundWithRandomPitch(clipName, 1f, 1f);
         }
-        
+
         public void PlaySoundWithRandomPitch(string clipName, float minPitch, float maxPitch)
         {
-            var clipInfo = sounds.Find(clipInfo => clipInfo.name == clipName);
+            var clipInfo = callableSounds.Find(clipInfo => clipInfo.name == clipName);
             if (clipInfo == null)
                 throw new KeyNotFoundException($"Not found '{clipName}' clip in registered list");
 
+            PlaySoundInternal(clipInfo.sound, minPitch, maxPitch, true);
+        }
+
+        private void PlaySoundInternal(AudioClip audioClip, float minPitch, float maxPitch, bool highPriority)
+        {
+            if (_audioSource.isPlaying && !highPriority)
+                return;
+            
             _audioSource.pitch = Random.Range(minPitch, maxPitch);
-            _audioSource.PlayOneShot(clipInfo.sound);
+            _audioSource.PlayOneShot(audioClip);
         }
     }
 }
